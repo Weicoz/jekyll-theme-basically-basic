d54d66befa0d296f205462b66a295d0ca50283d6
WEI-PC\WEI <weicoz@qq.com>
2017-11-08 17:22:38 +0800

    updata

--- assets/javascripts/get.js
+++ assets/javascripts/get.js
@@ -1,5 +1,5 @@
 ﻿
-getUrlParam = function (name) {
+function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return decodeURI(r[2]); return '';
